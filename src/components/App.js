e9aaf8d58817ebfdd6b1a520e90b263c8c122bce
haqnawaz <haqnawaz99@yahoo.com>
2022-07-06 16:44:48 +0500

    Header and Add contact added

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import AddContact from "./AddContact";
+import ContactList from "./ContactList";
 import "./App.css";
 
 function App() {
   return (
     <div>
-      <h1>Is it okay</h1>
+      <Header />
+      <AddContact />
     </div>
   );
 }
